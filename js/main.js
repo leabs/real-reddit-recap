023c739158fc57dfc9bc5a09c4cd17d2fe7a175f
Steve <stevenleabo@gmail.com>
2022-12-19 15:40:27 -0500

    adding user prompt

--- js/main.js
+++ js/main.js
@@ -1,14 +1,53 @@
-const redditFetch = require('reddit-fetch');
+//on page load hide page body
 
-redditFetch({
 
-    subreddit: 'all',
-    sort: 'hot',
-    allowNSFW: true,
-    allowModPost: true,
-    allowCrossPost: true,
-    allowVideo: true
+//prompt user to enter their username
+function promptUser() {
+  let username = prompt("Enter your Reddit username");
+  document.body.style.display = "none";
+  if (username != null) {
+    document.body.style.display = "block";
+    document.getElementById("username").innerHTML = username;
 
-}).then(post => {
-    console.table(post);
-});
+    document.getElementById("username").innerHTML = username;
+    //display top comment inside comment-wrapper div and display subreddit inside sub-reddit button
+    function displayTopComment() {
+      fetch(
+        "https://www.reddit.com/user/" +
+          username +
+          "/comments.json?sort=top&t=year&limit=1"
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          document.getElementById("comment").innerHTML =
+            data.data.children[0].data.body;
+          document.getElementById("sub-reddit").innerHTML =
+            data.data.children[0].data.subreddit;
+            document.getElementById("upvotes").innerHTML =
+            data.data.children[0].data.ups;
+        }),
+        fetch(
+          "https://www.reddit.com/user/" +
+            username +
+            "/submitted.json?sort=top&t=year&limit=1"
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            document.getElementById("post-title").innerHTML =
+              data.data.children[0].data.title;
+            document.getElementById("post-body").innerHtml =
+              '<img src="' + data.data.children[0].data.thumbnail + '"/>';
+
+            document.getElementById("sub-reddit").innerHTML =
+              data.data.children[0].data.subreddit;
+          });
+      let commentSub =
+        "https://www.reddit.com/r/" + data.data.children[0].data.subreddit;
+      document.getElementById("myLink").setAttribute("href", link);
+      document.getElementById("myLink").innerHTML = link;
+    }
+    displayTopComment();
+  }
+}
+
+promptUser();
