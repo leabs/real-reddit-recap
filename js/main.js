3d4091f01690a5ef519362edd87302567792376d
Steve <stevenleabo@gmail.com>
2022-12-28 11:20:15 -0500

    adding some meta, adjusting the ssubreddit button to just text

--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
-//on page load hide page body
-
-
 //prompt user to enter their username
 function promptUser() {
   let username = prompt("Enter your Reddit username");
 
   if (username != null) {
-
     document.getElementById("username").innerHTML = username;
 
     document.getElementById("username").innerHTML = username;
@@ -15,7 +11,7 @@ function promptUser() {
       fetch(
         "https://www.reddit.com/user/" +
           username +
-          "/comments.json?sort=top&t=year&limit=1"
+          "/comments.json?sort=top&t=year&limit=2"
       )
         .then((response) => response.json())
         .then((data) => {
@@ -23,7 +19,7 @@ function promptUser() {
             data.data.children[0].data.body;
           document.getElementById("sub-reddit").innerHTML =
             data.data.children[0].data.subreddit;
-            document.getElementById("upvotes").innerHTML =
+          document.getElementById("upvotes").innerHTML =
             data.data.children[0].data.ups;
         }),
         fetch(
@@ -35,12 +31,12 @@ function promptUser() {
           .then((data) => {
             document.getElementById("post-title").innerHTML =
               data.data.children[0].data.title;
-              document.getElementById("post-body").innerHTML =
+            document.getElementById("post-body").innerHTML =
               data.data.children[0].data.selftext_html;
             document.getElementById("comment-sub-reddit").innerHTML =
               data.data.children[0].data.subreddit;
-              document.getElementById("comment-upvotes").innerHTML =
-            data.data.children[0].data.ups;
+            document.getElementById("comment-upvotes").innerHTML =
+              data.data.children[0].data.ups;
           });
       let commentSub =
         "https://www.reddit.com/r/" + data.data.children[0].data.subreddit;
